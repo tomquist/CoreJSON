05059a29e2c095f15d1b4d99b2e9955fa057a160
tomquist <tomquist@users.noreply.github.com>
2016-12-19 10:26:48 +0100

    Added all tests to LinuxMain

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -3,4 +3,10 @@ import XCTest
 
 XCTMain([
     testCase(CoreJSONTests.allTests),
+    testCase(CoreJSONSubscriptTests.allTests),
+    testCase(CoreJSONPointerTests.allTests),
+    testCase(CoreJSONLiteralsTests.allTests),
+    testCase(CoreJSONFromFoundationTests.allTests),
+    testCase(CoreJSONToFoundationTests.allTests),
+    testCase(CoreJSONConvenienceTests.allTests),
 ])
